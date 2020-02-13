3735c5fef8b21c69761d17a5b00848d5cd5139f5
Marno <790017595@qq.com>
2020-02-14 02:30:25 +0800

    样式修改

--- pages/organize/organize.js
+++ pages/organize/organize.js
@@ -6,13 +6,28 @@ Page({
    */
   data: {
     ifdelay:false,
-    timelong:30
+    timelong:30,
+    height: 1000,
+    currentTab:0,
+    bt:true,
+    motto:"正在搜索附近的蓝牙信标..."
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let windowHeight = wx.getSystemInfoSync().windowHeight; // 屏幕的高度
+    console.log(windowHeight)
+    let query = wx.createSelectorQuery();
+    var that = this
+    query.select('.formHead').boundingClientRect(rect => {
+      let contheight = windowHeight - rect.height;
+      that.setData({
+        height: contheight
+      });
+    }).exec();
+    
 
   },
 
@@ -71,5 +86,12 @@ Page({
 
   settimelong(e){
     this.setData({ timelong: e.detail.value })
+  },
+
+  nextstep(e){
+    this.setData({
+      currentTab: e.target.dataset.current
+    })
   }
+
 })
