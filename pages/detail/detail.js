e1ea7614d5770033779a81af21f0c265fa0979b6
Marno Yip <790017595@qq.com>
2020-03-15 01:50:49 +0800

    改用tab形式，增加下拉刷新

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -7,29 +7,69 @@ Page({
    */
   data: {
     way: "",
+    width:400,
     height: 1000,
     current: 0,
+    listNull:false,
     sceneListReady: [],
     sceneListDone: [],
-    
+    triggered:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+
+    let windowHeight = wx.getSystemInfoSync().windowHeight; // 屏幕的高度
+    let query = wx.createSelectorQuery();
+    query.select('.tab').boundingClientRect(rect => {
+      let contantheight = windowHeight - rect.height;
+      this.setData({
+        height: contantheight,
+        width: wx.getSystemInfoSync().windowWidth / 2,
+      });
+      console.log(">>>" + rect.height)
+    }).exec();
     this.setData({
-        height: wx.getSystemInfoSync().windowHeight,
         way : options.way
     });
-    console.log(options.way)
+    console.log(wx.getSystemInfoSync().windowWidth)
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
+    setTimeout(() => {
+      this.setData({
+        triggered: true,
+      })
+    }, 50)
+  },
 
+  onPulling(e) {
+    console.log('onPulling:', e)
+  },
+
+  onRefresh() {
+    if (this._freshing) return
+    this._freshing = true
+    setTimeout(() => {
+      this.setData({
+        triggered: false,
+      })
+      this._freshing = false
+    }, 1000)
+  },
+
+  onRestore(e) {
+    console.log('onRestore:', e)
+  },
+
+  onAbort(e) {
+    console.log('onAbort', e)
   },
 
   /**
@@ -96,7 +136,8 @@ Page({
           console.log(data.data)
           if (data.data){
             that.setData({
-              current: 1
+              current: 1,
+              listNull:true
             })
           }else{
             that.setData({
@@ -135,6 +176,22 @@ Page({
   scene: function(e) {
     wx.navigateTo({
       url: '../scene/scene?sceneID=' + e.currentTarget.dataset.id
+    })
+  },
+
+  currentChanged(e){
+    var that = this
+    if (e.detail.source == "touch"){
+      that.setData({
+        current: e.detail.current
+      })
+    }
+    
+  },
+
+  clicktab:function(e){
+    this.setData({
+      current: e.currentTarget.dataset.id
     })
   }
 })
